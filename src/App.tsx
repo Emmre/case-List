8966460f18d5ff3ad999cbbd882032ba2c1bc4d6
Emre <emosrc43@gmail.com>
2021-12-25 15:19:54 +0300

    Case added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import Button from './components/Button'
+import React, { Fragment } from "react";
+import { data } from "./services/data";
 
 const App = () => {
   return (
     <div>
-      sa
-      <Button>Selam</Button>
+      {Object.entries(data).map((item, index) => {
+        const { name, userCreated }: any = item[1];
+        return (
+          <ul key={index}>
+            {item[0]}
+            {Array.isArray(item[1]) ? (
+              item[1]
+                .sort((a, b) =>
+                  a.dateCreated > b.dateCreated || a.name > b.name
+                    ? 1
+                    : b.dateCreated > a.dateCreated || b.name > a.name
+                    ? -1
+                    : 0
+                )
+                .map((val, idx) => {
+                  return (
+                    <Fragment key={idx}>
+                      {!val.isArchived && (
+                        <li>{val.name ? val.name : val.content}</li>
+                      )}
+                    </Fragment>
+                  );
+                })
+            ) : (
+              <Fragment>
+                <div>Name: {name}</div>
+                <div>User Created :{userCreated}</div>
+              </Fragment>
+            )}
+          </ul>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
